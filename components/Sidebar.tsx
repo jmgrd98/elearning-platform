a302ac8ca2d9bd437b6c68269285e511bf5dd592
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-03 19:18:33 -0300

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,23 +1,42 @@
 'use client'
 
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { useState } from 'react';
+import { Button } from './ui/button';
 
 
 const Sidebar = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleLinkClick = (href: string) => {
     router.push(href);
   };
 
+  const onSubscribe = async () => {
+    try {
+        setLoading(true);
+        const response = await axios.get('/api/stripe');
+
+        window.location.href = response.data.url;
+    } catch (error) {
+        console.error(error, 'STRIPE_CLIENT_ERROR')
+    } finally {
+        setLoading(false);
+    }
+};
+
   return (
-    <div className="w-1/5 min-h-screen left-0 bg-black text-white p-5">
-      <p className="font-bold text-2xl mb-10">Formando Creators</p>
+    <div className="w-1/5 min-h-screen left-0 bg-black text-white p-5 justify-between">
+      <p onClick={() => handleLinkClick('/')} className="cursor-pointer font-bold text-2xl mb-10">Formando Creators</p>
       <div className="flex flex-col gap-3 items-left">
-        <button onClick={() => handleLinkClick('/lesson1')} className="hover:bg-white hover:text-black p-2 rounded w-full">Aula 1</button>
-        <button onClick={() => handleLinkClick('/lesson2')} className="hover:bg-white hover:text-black p-2 rounded w-full">Aula 2</button>
-        <button onClick={() => handleLinkClick('/lesson3')} className="hover:bg-white hover:text-black p-2 rounded w-full">Aula 3</button>
+        <Button variant={'secondary'} onClick={() => handleLinkClick('/lesson1')} >Aula 1</Button>
+        <Button variant={'secondary'} onClick={() => handleLinkClick('/lesson2')} >Aula 2</Button>
+        <Button variant={'secondary'} onClick={() => handleLinkClick('/lesson3')} >Aula 3</Button>
       </div>
+
+      <Button variant={'destructive'} onClick={onSubscribe} >Matricule-se</Button>
     </div>
   );
 };
