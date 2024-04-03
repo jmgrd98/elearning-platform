eebb50c1c6d40fe0b9b04f43e4a2bf82ddf85495
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-03 20:09:06 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import Video from 'next-video';
-// import getStarted from 'https://youtu.be/tPcUszOmU24'
+import getStarted from '../videos/get-started.mp4.json'
 import { useUser } from "@clerk/nextjs"
 
-const getStarted = {
+const video = {
   url: '../videos/get-started.mp4.json',
   // poster: 'https://example.com/poster.jpg',
 };
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="p-5 text-center">
       <h1 className='text-5xl font-bold mb-5'>Olá, {user?.firstName}</h1>
-      <Video width={700} height={450} src={getStarted.url} />
+      <Video width={700} height={450} src={video.url} />
     </div>
   );
 }
