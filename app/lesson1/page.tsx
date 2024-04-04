e010f59deb10b5903321942c280d4eac2fd3ca20
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-04 19:44:19 -0300

    Update page.tsx

--- app/lesson1/page.tsx
+++ app/lesson1/page.tsx
@@ -26,7 +26,7 @@ const page = () => {
     <div className='p-5 text-center flex flex-col items-center'>
       <h1 className='text-5xl font-bold mb-5'>Aula 1</h1>
       {/* <Video width={700} height={450} className='m-0' src={video.url} /> */}
-      <iframe src='https://www.youtube.com/embed?v=iu-LBY7NXD4' width={700} height={400} />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/iu-LBY7NXD4?si=N9TO0blaPMhOH4ZP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       <div className='flex flex-col gap-5 mt-5'>
         <p className='text-xl font-bold'>Tem alguma dúvida sobre essa aula? Compartilhe conosco e iremos te ajudar!</p>
