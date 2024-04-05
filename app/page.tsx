1c98815a0bc1c34973a2cbff9fc54f5e673b2d55
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-05 15:25:59 -0300

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,73 @@ import { Button } from "@/components/ui/button";
 import { useUserProgress } from "../context/ProgressContext";
 import { useEffect } from 'react';
 import { useUser } from '@clerk/nextjs';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Iframe from 'react-iframe'
+
 
 export default function Home() {
   const { user } = useUser();
   const { progress, setProgress } = useUserProgress();
   
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500)
+    const timer = setTimeout(() => setProgress(100), 500)
     return () => clearTimeout(timer)
-  }, [setProgress])
+  }, [])
 
   return (
-    <div className="p-5 text-center flex flex-col gap-20 items-center">
+    <div className="p-5 text-center flex flex-col gap-10 items-center">
       <h1 className='text-5xl font-bold'>Olá, {user?.firstName}!</h1>
 
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 w-full items-center">
         <p className="text-2xl font-bold">Esse é o seu progesso no curso</p>
         <Progress value={progress} className="w-[60%]" />
       </div>
+
+      <Carousel >
+        <CarouselContent className='max-w-[800px]' >
+          <CarouselItem className=' flex justify-center items-center'>
+              <Iframe
+            url="https://www.youtube.com/embed?v=tPcUszOmU24"
+            width="480px"
+            height="240px"
+            id=""
+            className=""
+            display="block"
+            position="relative"
+          />
+          </CarouselItem>
+          <CarouselItem className=' flex justify-center items-center'>
+          <Iframe
+            url="https://www.youtube.com/embed?v=tPcUszOmU24"
+            width="480px"
+            height="240px"
+            id=""
+            className=""
+            display="block"
+            position="relative"
+          />
+          </CarouselItem>
+          <CarouselItem className=' flex justify-center items-center'>
+          <Iframe
+            url="https://www.youtube.com/embed?v=tPcUszOmU24"
+            width="480px"
+            height="240px"
+            id=""
+            className=""
+            display="block"
+            position="relative"
+          />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
 
       <Button variant={'purple'}>Continuar de onde parei</Button>
     </div>
