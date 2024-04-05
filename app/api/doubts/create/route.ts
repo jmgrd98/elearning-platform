a1fad59d85a456ec3e5ac1b7b20d9c4267dcfa3d
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-05 15:49:13 -0300

    Update route.ts

--- app/api/doubts/create/route.ts
+++ app/api/doubts/create/route.ts
@@ -1,14 +1,25 @@
 import prismadb from "@/lib/prismadb";
 
+interface DoubtCreateInput {
+    userId: string;
+    content: string;
+    aulaId: number;
+}
+
 export const POST = async (req: any) => {
     try {
-        const {userId, content} = await req.json();
+        const { userId, content, aulaId } = await req.json();
+        
+        const data: DoubtCreateInput = {
+            userId,
+            content,
+            aulaId
+        };
+
         const newDoubt = await prismadb.doubt.create({
-            data: {
-                userId,
-                content
-            },
+            data,
         });
+
         return new Response(JSON.stringify(newDoubt), { status: 201 });
     } catch (error) {
         return new Response(JSON.stringify(error), { status: 500 });
