340467c792462c11f1433a0034dd7aa3ec3af5fd
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-07 18:19:09 -0300

    youtube api rapid hub alternative

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     env: {
         YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        X_RAPIDAPI_KEY: process.env.X_RAPIDAPI_KEY,
+        X_RAPIDAPI_HOST: process.env.X_RAPIDAPI_HOST
     }
 };
 
