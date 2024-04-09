fff945fc2b9018d8fe339eee5b2999fce87faa39
João Marcelo Dantas <jmgrd98@gmail.com>
2024-04-09 10:11:14 -0300

    Update page.tsx

--- app/(inside)/comunidade/page.tsx
+++ app/(inside)/comunidade/page.tsx
@@ -12,7 +12,12 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogFooter
 } from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const page = () => {
@@ -31,37 +36,64 @@ const page = () => {
     };
 
     fetchPosts();
-  }, [])
+  }, []);
+
+  const createPost = async () => {
+    try {
+      await axios.post('/api/posts/create');
+      toast.success('Post criado com sucesso!');
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
   return (
-    <div className="p-5 text-center flex flex-col gap-10 items-center">
-      <h1 className='text-4xl font-bold'>Comunidade</h1>
+    <>
+      <div className="p-5 text-center flex flex-col gap-10 items-center">
+        <h1 className='text-4xl font-bold'>Comunidade</h1>
 
-      <Dialog>
-          <DialogTrigger asChild>
-            <Button variant={'purple'} className='rounded-full p-3 cursor-pointer'>
-              Criar Post
-            </Button>
-            </DialogTrigger>
-          <DialogContent className='h-full max-h-[600px]' onCloseAutoFocus={(e) => e.preventDefault()}>
-            <DialogHeader>
-              <DialogTitle className='mb-5'>Tire sua dúvida com o Luide AI! 🤖</DialogTitle>
-              <DialogDescription>
+        <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={'purple'} className=' rounded p-3 cursor-pointer'>
+                Criar post
+              </Button>
+              </DialogTrigger>
+            <DialogContent className='h-full max-h-[600px]' onCloseAutoFocus={(e) => e.preventDefault()}>
+              <DialogHeader>
+                <DialogTitle className='text-xl font-bold mb-5'>Criar post</DialogTitle>
+                <DialogDescription className='flex flex-col items-center gap-5'>
+                  <form>
+                    <Input placeholder="Título" className='w-1/2' />
+                    <Textarea placeholder="Conteudo" className='w-1/2' />
+                    <Button onClick={() => createPost()} className="w-1/2">Enviar</Button>
+                  </form>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+        </Dialog>
 
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-      </Dialog>
+        <p>Interaja com a comunidade de Creators!</p>
 
-      <p>Interaja com a comunidade de Creators!</p>
+        <Input placeholder="Pesquise por posts, tags ou usuários..." className='w-1/2' />
 
-      <Input placeholder="Pesquise por posts, tags ou usuários..." className='w-1/2' />
-
-      {posts.map((post: any) => (
-        <PostCard key={post.id} post={post} />
-      ))}
-    </div>
+        {posts.map((post: any) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+      <ToastContainer
+                  position="bottom-left"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick={true}
+                  rtl={false}
+                  pauseOnFocusLoss={false}
+                  draggable={true}
+                  pauseOnHover={true}
+                  className={'z-0'}
+              />
+    </>
   )
 }
 
